69f9463a3f12ece49da0e3c3208e69331a29bf3f
Fan <fsjwlf@126.com>
2024-11-14 17:41:30 +0800

    Update ExportMeshData.py

--- Postprocessing/ExportMeshData.py
+++ Postprocessing/ExportMeshData.py
@@ -10,7 +10,9 @@ def LoadData(filename):
 
 
 def ExportMesh(res):
-    # 输出节点Node、单元Element个数；输出节点坐标；输出单元索引
+    # 输出节点Node、单元Element个数
+    # 输出节点坐标
+    # 输出单元索引
     mesh = res.tri  # 获取网格
     x, y, triangles = mesh.x, mesh.y, mesh.triangles  # 节点经纬度和单元
     f = open('mesh.txt', 'w')
@@ -18,10 +20,10 @@ def ExportMesh(res):
     print(f'Node {len(x)} Element {len(triangles)}', file=f)
     # 输出节点坐标
     for i in range(len(x)):
-        print(f'{i}\t{x[i]}\t{y[i]}', file=f)
+        print(f'{x[i]}\t{y[i]}', file=f)
     # 输出单元节点索引
     for i in range(len(triangles)):
-        print(f'{i}\t{triangles[i][0]}\t{triangles[i][1]}\t{triangles[i][2]}', file=f)
+        print(f'{triangles[i][0]}\t{triangles[i][1]}\t{triangles[i][2]}', file=f)
     f.close()
 
 
@@ -30,12 +32,14 @@ def ExportPointValue(res, varname, time):
     value = res.get_data_value(varname, time)  # 获取time时间的变量值
     f = open(f'point_{varname}_{time}.txt', 'w')
     for i in range(len(value)):
-        print(f'{i}\t{value[i]}', file=f)
+        print(f'{value[i]}', file=f)
     f.close()
 
 
 def ExportMeshValue(res, varname, time):
-    # 输出节点Node、单元Element个数；输出节点坐标和值；输出单元索引
+    # 输出节点Node、单元Element个数
+    # 输出节点坐标和值
+    # 输出单元索引
     mesh = res.tri  # 获取网格
     x, y, triangles = mesh.x, mesh.y, mesh.triangles  # 节点经纬度和单元
     value = res.get_data_value(varname, time)  # 获取time时间的变量值
@@ -44,16 +48,14 @@ def ExportMeshValue(res, varname, time):
     print(f'Node {len(x)} Element {len(triangles)}', file=f)
     # 输出节点坐标和节点值
     for i in range(len(x)):
-        print(f'{i}\t{x[i]}\t{y[i]}\t{value[i]}', file=f)
+        print(f'{x[i]}\t{y[i]}\t{value[i]}', file=f)
     # 输出单元节点索引
     for i in range(len(triangles)):
-        print(f'{i}\t{triangles[i][0]}\t{triangles[i][1]}\t{triangles[i][2]}', file=f)
+        print(f'{triangles[i][0]}\t{triangles[i][1]}\t{triangles[i][2]}', file=f)
     f.close()
 
 
 res = LoadData('out-result.slf')
-
 ExportMesh(res)  # 输出网格结构 mesh.txt
 ExportPointValue(res, 'WATER DEPTH', 40)  # 输出带节点值的网格结构
-
 ExportMeshValue(res, 'WATER DEPTH', 40)  # 输出带节点值的网格结构
